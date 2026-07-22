f1879672be5b93470744781a4ab80cd7be3ceca3
Cristian Tone <nelu-cristian.tone@adesso-romania.ro>
2026-07-22 09:43:51 +0300

    Fixed bug about buy now button

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -9,9 +9,9 @@ const HomePage = () => {
     <div>
       {/**Hero section */}
       <HeroSection
-        title="Buy iPhone 18 Pro"
+        title="Buy iPhone 14 Pro"
         subtitle="Experience the power of the latest iPhone 18 with our most Pro camera ever."
-        link="/product/6a4e034692dbbe7cbbc5a5ef"
+        link="/product/6a5f32bff519e85c3173d8ff"
         image={iphone}
       />
 
@@ -22,7 +22,7 @@ const HomePage = () => {
       <HeroSection
         title="Build the ultimate setup"
         subtitle="You cand add Studio Display and colour-matched Magic accessories to your bag after configure your Mac mini."
-        link="/product/6a4e034692dbbe7cbbc5a5f7"
+        link="/product/6a5f32c0f519e85c3173d907"
         image={mac}
       />
     </div>
